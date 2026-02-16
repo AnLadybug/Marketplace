a6ed4824280d01049cca0d34b6b4644956082f78
AnKo <korans1997@mail.ru>
2026-02-16 23:17:29 +0300

    Интерфейс взаимодейтсвия с элементами каталога

---
+++ src/Services/Catalog/Catalog.Domain/Repositoties/ICatalogItemRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Domain.Repositoties
+{
+    internal interface ICatalogItemRepository
+    {
+        Task<CatalogItem> CreateCatalogItemAsync(CatalogItem item);
+        Task<IEnumerable<CatalogItem>> GetCatalogItemsAsync();
+        Task<CatalogItem?> GetCatalogItemAsync(Guid id);
+    }
+}
