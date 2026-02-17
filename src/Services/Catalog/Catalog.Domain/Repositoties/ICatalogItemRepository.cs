d76284ae9a41d7a26d0273db6b18fd1831ce6a72
AnKo <korans1997@mail.ru>
2026-02-17 19:13:01 +0300

    Добавлен интерфейс для работы с элементами каталога

--- src/Services/Catalog/Catalog.Domain/Repositoties/ICatalogItemRepository.cs
+++ src/Services/Catalog/Catalog.Domain/Repositoties/ICatalogItemRepository.cs
@@ -5,5 +5,9 @@
         Task<CatalogItem> CreateCatalogItemAsync(CatalogItem item);
         Task<IEnumerable<CatalogItem>> GetCatalogItemsAsync();
         Task<CatalogItem?> GetCatalogItemAsync(Guid id);
+        Task<IEnumerable<CatalogItem>> GetCatalogItemsByTitleAsync(string title);
+        Task<IEnumerable<CatalogItem>> GetCatalogItemsByBrandAsync(string brandTitle);
+        Task<bool> UpdateCatalogItemAsync(CatalogItem item);
+        Task<bool> DeleteCatalogItemAsync(Guid id);
     }
 }
